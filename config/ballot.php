5e89393c5adaa961caf927ae42f62e8d760fe075
Lester Hurtado <lester@hurtado.ph>
2019-06-17 07:11:39 +0800

    modify config re: image destination

--- config/ballot.php
+++ config/ballot.php
@@ -8,7 +8,7 @@ return [
 		'image' => [
 			'seed' => env('IMAGE_SEED', 'tests/files/image.jpg'),
 			'source' => env('BALLOT_SOURCE', storage_path('app/public/ballot.jpg')),
-			'destination' => env('BALLOT_DESTINATION', storage_path('app/ballot.jpg')),
+			'destination' => env('BALLOT_DESTINATION', storage_path('app/public/ballot.jpg')),
 			'qrcode' => env('BALLOT_QRCODE', storage_path('app/qrcode.jpg')),
 		],
 		'temp' => storage_path('app')
